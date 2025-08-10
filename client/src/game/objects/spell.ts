5a3cc78031acd09e656e47a6a33bf5e02b3196e5
miles-evan <miles.todtfeld@gmail.com>
2025-08-10 16:51:24 -0400

    implemented syncing movment

--- client/src/game/objects/spell.ts
+++ client/src/game/objects/spell.ts
@@ -10,7 +10,9 @@ export default class Spell extends GameObject {
 	
 	tier: 1 | 2 | 3 | 4; // the spell's number
 	player: 1 | 2; // player 1 is bottom-up, player 2 is top-down
-	readonly vy = 0.05; // pixels per millisecond
+	static readonly vy = 0.05; // pixels per millisecond
+	static readonly framesPerTick = Math.round(64 / Spell.vy / 1000 * Game.maxFrameRate);
+	moving: boolean = false; // whether it's moving (only starts moving on tick start)
 
 	constructor(x: number, y: number, tier: 1|2|3|4, player: 1|2){
 		super(x, y, 64, 64, [s1, s2, s3, s4][tier-1]);
@@ -20,8 +22,13 @@ export default class Spell extends GameObject {
 
 	step() {
 		
-		this.y += this.vy * Game.deltaTime * (this.player === 1? -1 : 1);
-		if(this.top > Game.screenHeight || this.bottom < 0)
-			this.destroy();
+		if(Game.frameCount % Spell.framesPerTick === 0)
+			this.moving = true;
+		if(this.moving) {
+			this.y += Spell.vy * Game.deltaTime * (this.player === 1? -1 : 1);
+			if(this.top > Game.screenHeight || this.bottom < 0)
+				this.destroy();
+		}
+		
 	}
 }
