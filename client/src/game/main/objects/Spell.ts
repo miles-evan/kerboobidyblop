ec33c113bf12d7366c5bac22647016aad7fcd47e
kevdarby <Darbykevin2@gmail.com>
2025-08-12 22:01:45 -0400

    powers

--- client/src/game/main/objects/Spell.ts
+++ client/src/game/main/objects/Spell.ts
@@ -15,30 +15,54 @@ export default class Spell extends GameObject {
 	static readonly vy = 0.05; // pixels per millisecond
 	static readonly framesPerTick = Math.round(64 / Spell.vy / 1000 * Game.maxFrameRate);
 
-	tierEliminationMap = { // map of which spells beat who
+	static readonly tierEliminationMap = { // map of which spells beat who
 		1: [4],
 		2: [1],
 		3: [2, 1],
 		4: [3, 2, 1] 
 	};
+	// powers are function pointers that are called in order at the start of each step.
+	powers: Array<() => void> = [];
+
 
 	constructor(x: number, y: number, lane: Lane, tier: Tier, playerNum: PlayerNum){
 		super(x, y, 64, 64, [s1, s2, s3, s4][tier-1]);
 		this.lane = lane;
 		this.tier = tier;
 		this.playerNum = playerNum;
+		this.powers = [this.dodger];
 	}
 
-	handleCollisions() {
-		const colliders = Game.getObjectsCollisionsWithType(this, Spell)
+	dodger = () => {
+		const colliders = Game.getObjectsCollisionsWithType(this, Spell);
 		colliders.forEach(collider => {
-			if(this.tierEliminationMap[this.tier].includes(collider.tier)){
+			if(collider.kills(this)){
+				this.y += (this.playerNum === 1 ? 1 : -1) * 100;
+				// Remove the power once it's used once
+				this.powers = this.powers.filter(power => power !== this.dodger);
+			}
+		});
+	}
+
+	// Returns true if this kills collider
+	kills(collider: Spell) {
+		return Spell.tierEliminationMap[this.tier].includes(collider.tier);
+	}
+
+
+
+	handleCollisions() {
+		const colliders = Game.getObjectsCollisionsWithType(this, Spell);
+		colliders.forEach(collider => {
+			if(this.kills(collider)) {
 				collider.destroy();
 			}
 		});
 	}
 
 	step() {
+		this.powers.forEach(power => power());
+
 		this.handleCollisions();
 
 		if(Game.frameCount % Spell.framesPerTick === 0)
