2c300b5297fcff45fb9bc27fbb99eb2c2df3b913
miles-evan <miles.todtfeld@gmail.com>
2025-08-15 01:09:00 -0400

    added dodger, added mouse

--- client/src/game/main/objects/Spell.ts
+++ client/src/game/main/objects/Spell.ts
@@ -1,6 +1,7 @@
 import GameObject from "../../engine/GameObject.ts";
 import Game from "../../engine/Game.ts";
 import SpellTrail from "./SpellTrail.ts";
+import type Board from "./Board.ts";
 
 
 export default class Spell extends GameObject {
@@ -9,18 +10,19 @@ export default class Spell extends GameObject {
 	readonly tier: Tier; // the spell's number
 	power: Power;
 	readonly playerNum: PlayerNum; // player 1 is bottom-up, player 2 is top-down
-	moving: boolean = false; // whether it's moving (only starts moving on tick start)
-	moveDirection: 1 | -1;
-	static readonly velocity = 0.006; // pixels per millisecond
-	static readonly framesPerTick = Math.round(16 / Spell.velocity / 1000 * Game.maxFrameRate);
+	moveDirectionX: -1 | 0 | 1 = 0;
+	moveDirectionY: -1 | 0 | 1 = 0;
+	board: Board;
+	static readonly velocity: number = 0.006; // pixels per millisecond
+	static readonly framesPerTick: number = Math.round(16 / Spell.velocity / 1000 * Game.maxFrameRate);
 	
-	constructor(x: number, y: number, lane: Lane, tier: Tier, playerNum: PlayerNum, power: Power = "none") {
+	constructor(x: number, y: number, lane: Lane, tier: Tier, playerNum: PlayerNum, power: Power = "none", board: Board) {
 		super(x, y, 16, 16, `/src/game/main/sprites/spells/spell-player${playerNum}-tier${tier}.png`);
 		this.lane = lane;
 		this.tier = tier;
 		this.playerNum = playerNum;
-		this.moveDirection = playerNum === 1? -1 : 1;
 		this.power = power;
+		this.board = board;
 	}
 
 	static readonly tierEliminationMap = { // map of which spells beat who
@@ -35,27 +37,53 @@ export default class Spell extends GameObject {
 		return other.playerNum !== this.playerNum
 			&& Spell.tierEliminationMap[this.tier].includes(other.tier);
 	}
+	
+	collidedWithEnemy(x?: number, y?: number): boolean {
+		return this.getCollisionsWithType(Spell, x, y).some(collider => this.playerNum !== collider.playerNum);
+	}
+	
+	collidedWithAlly(x?: number, y?: number): boolean {
+		return this.getCollisionsWithType(Spell, x, y).some(collider => this.playerNum === collider.playerNum);
+	}
 
 	retreater(): void {
-		// set to forward, only if lined up with a tile and not colliding with an ally
-		if(Game.frameCount % Spell.framesPerTick === 0
-			&& !this.getCollisionsWithType(Spell).some(collider => this.playerNum === collider.playerNum))
-			this.moveDirection = this.playerNum === 1? -1 : 1;
+		// stop retreating if lined up with a tile and not colliding with an ally
+		if(Game.frameCount % Spell.framesPerTick === 0 && !this.collidedWithAlly())
+			this.moveDirectionY = this.playerNum === 1? -1 : 1;
 		
-		const colliders: Spell[] = this.getCollisionsWithType(Spell, this.x, this.y + 16 * this.moveDirection);
+		const colliders: Spell[] = this.getCollisionsWithType(Spell, this.x, this.y + 16 * this.moveDirectionY);
 		colliders.forEach(collider => {
 			if(collider.kills(this))
-				this.moveDirection = this.playerNum === 1? 1 : -1; // turn around
+				this.moveDirectionY = this.playerNum === 1? 1 : -1; // turn around
 		});
 	}
 	
 	
 	dodger(): void {
-	
+		// im gonna extract out the logic for changing lanes once i get to this
 	}
 	
+	private wasLinedUpLastFrame: boolean = true;
 	hopper(): void {
-	
+		if(!this.moveDirectionY) return;
+		
+		// stop hopping if lined up with square
+		const isLinedUp: boolean = this.board.positionLinesUpWithTile(this.x, undefined, true);
+		if(!this.wasLinedUpLastFrame && isLinedUp) {
+			this.moveDirectionX = 0;
+			this.x = Math.round(this.x);
+		}
+		this.wasLinedUpLastFrame = isLinedUp;
+		
+		// start hopping if there are any enemies diagonally, no allies next to you there, and you're not alr hopping
+		([1, -1] as const).forEach(dir => {
+			if(
+				this.collidedWithEnemy(this.x + 16*dir, this.y + 32 * this.moveDirectionY)
+				&& !this.collidedWithAlly(this.x + 16*dir, this.y)
+			) {
+				this.moveDirectionX = dir;
+			}
+		});
 	}
 
 
@@ -74,16 +102,17 @@ export default class Spell extends GameObject {
 		this.handleCollisions();
 		
 		if(Game.frameCount % Spell.framesPerTick === 0) // start moving when lined up with a tile
-			this.moving = true;
+			this.moveDirectionY = this.playerNum === 1? -1 : 1;
 		
-		if(this.moving) {
-			this.y += Spell.velocity * Game.deltaTime * this.moveDirection;
-			if(this.top > Game.screenHeight || this.bottom < 0)
-				this.destroy();
-			
-			if(Game.frameCount % Math.round(Spell.framesPerTick / 16))
-				new SpellTrail(this);
-		}
+		this.x += Spell.velocity * Game.deltaTime * this.moveDirectionX;
+		this.y += Spell.velocity * Game.deltaTime * this.moveDirectionY;
 		
+		
+		if(this.top > Game.screenHeight || this.bottom < 0)
+			this.destroy();
+		
+		if((this.moveDirectionY || this.moveDirectionX) && Game.frameCount % Math.round(Spell.framesPerTick / 16))
+			new SpellTrail(this);
 	}
+	
 }
